dc173116ece3daa895026c945db95380b6f88858
yusuke matsumoto <b0949ym@gmail.com>
2025-08-04 00:25:30 +0900

    feat: Remove old backend directory

--- backend_old/src/utils/db.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    logging: false, // ログを無効化
-});
-
-module.exports = sequelize;
